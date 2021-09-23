b7cf9ea88bffc8912e1c6766b9fe9a2dce92a0c9
zeromsi <www.zeromsi2@gmail.com>
2021-09-23 13:25:01 +0600

    refactore

--- core/v1/resource.go
+++ core/v1/resource.go
@@ -5,6 +5,7 @@ import "github.com/klovercloud-ci/enums"
 type Resource struct {
 	Step string `json:"step"`
 	ProcessId string `json:"process_id"`
+	Descriptors *[]interface{} `json:"descriptors" yaml:"descriptors"`
 	Type     enums.RESOURCE_TYPE `json:"type"`
 	Name string                  `json:"name"`
 	Namespace string             `json:"namespace"`
